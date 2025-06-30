0a6e84437955c7acf4ea87b06177da68600b0442
Alexis <phucni2004@gmail.com>
2025-06-30 10:30:15 +0700

    initial setup

---
+++ write-it/src/main/java/com/writeit/write_it/user/UserController.java
@@ -0,0 +1,13 @@
+package com.writeit.write_it.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    
+    @RequestMapping("/user")
+    public String index() {
+        return "Welcome to UserPage!";
+    }
+}
---
+++ write-it/src/test/java/com/writeit/write_it/WriteItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writeit.write_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
