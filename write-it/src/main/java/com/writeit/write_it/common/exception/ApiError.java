fe44d9db516eaa678eb4d32b3ec8f70358ac359b
Alexis <phucni2004@gmail.com>
2025-09-10 11:30:26 +0700

    fix some logic bugs for the update-password function

--- write-it/src/main/java/com/writeit/write_it/common/exception/ApiError.java
+++ write-it/src/main/java/com/writeit/write_it/common/exception/ApiError.java
@@ -9,11 +9,13 @@ public enum ApiError {
 
     AUTHENTICATION_FAILED("AUTH_001", HttpStatus.UNAUTHORIZED, "Authentication failed."),
     ACCESS_DENIED       ("AUTH_002", HttpStatus.FORBIDDEN, "Access denied."),
-    INVALID_CREDENTIALS ("AUTH_003", HttpStatus.UNAUTHORIZED, "Invalid username or password."),
+    CREDENTIALS_INVALID ("AUTH_003", HttpStatus.UNAUTHORIZED, "Invalid username or password."),
     USER_DEACTIVATED    ("AUTH_004", HttpStatus.FORBIDDEN, "User is disabled"),
     ACCOUNT_LOCKED      ("AUTH_005", HttpStatus.FORBIDDEN, "Account is locked"),
     ACCOUNT_EXPIRED     ("AUTH_006", HttpStatus.FORBIDDEN, "Account is expired"),
     CREDENTIALS_EXPIRED ("AUTH_007", HttpStatus.FORBIDDEN, "Credentials are expired"),
+    CURRENT_PASSWORD_INVALID ("AUTH_008", HttpStatus.UNPROCESSABLE_ENTITY, "Current password is incorrect."),
+    NEW_PASSWORD_SAME_AS_OLD ("AUTH_009", HttpStatus.UNPROCESSABLE_ENTITY, "New password is same as old password."),
 
     
     VALIDATION_FAILED   ("REQ_001", HttpStatus.BAD_REQUEST, "Validation failed."),
